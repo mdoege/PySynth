4c911e292f0326b0fb7b957b8c21e9a5813f22b2
Martin C. Doege <mdoege@compuserve.com>
2011-04-30 03:50:00 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name="PySynth",
-        version="0.8.2",
+        version="0.9",
         description="A simple music synthesizer for Python",
         author="Martin C. Doege",
         author_email="mdoege@compuserve.com",
 	url="http://home.arcor.de/mdoege/pysynth/",
-        py_modules=["pysynth", "pysynth_b", "pysynth_s"],
-	scripts=["read_abc.py"],
+        py_modules=["pysynth", "pysynth_b", "pysynth_s", "pysynth_beeper"],
+	scripts=["read_abc.py", "nokiacomposer2wav.py", "test_nokiacomposer2wav.py"],
 )
