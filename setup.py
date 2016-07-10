1991d9ff3f253b2d8c833a5d942c175f781290cb
Martin Doege <mdoege@compuserve.com>
2016-07-10 10:15:40 +0200

    fix URL

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name="PySynth",
         description="A simple music synthesizer for Python",
         author="Martin C. Doege",
         author_email="mdoege@compuserve.com",
-	url="http://home.arcor.de/mdoege/pysynth/",
+	url="http://mdoege.github.io/PySynth/",
         py_modules=["pysynth", "pysynth_b", "pysynth_s", "pysynth_e", "pysynth_beeper","play_wav"],
 	scripts=["read_abc.py", "nokiacomposer2wav.py", "test_nokiacomposer2wav.py", "menv.py", "mixfiles.py"],
 )
