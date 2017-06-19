6afa65c056717482971efb70d11b366d0fbfd639
Martin Doege <mdoege@compuserve.com>
2017-06-19 21:28:17 +0200

    add subtractive synth

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name="PySynth",
         author="Martin C. Doege",
         author_email="mdoege@compuserve.com",
 	url="http://mdoege.github.io/PySynth/",
-        py_modules=["pysynth", "pysynth_b", "pysynth_s", "pysynth_e", "pysynth_beeper", "play_wav", "mixfiles", "mkfreq", "demosongs"],
+        py_modules=["pysynth", "pysynth_b", "pysynth_c", "pysynth_s", "pysynth_e", "pysynth_beeper", "play_wav", "mixfiles", "mkfreq", "demosongs"],
 	scripts=["read_abc.py", "readmidi.py", "nokiacomposer2wav.py", "test_nokiacomposer2wav.py", "menv.py"],
 )
 
