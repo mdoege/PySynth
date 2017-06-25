55cf21f7ab14cff671f422d9a2806de53fb46878
Martin Doege <mdoege@compuserve.com>
2017-06-25 13:18:20 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name="PySynth",
-        version="2.1",
+        version="2.2",
         description="A simple music synthesizer for Python 3",
         author="Martin C. Doege",
         author_email="mdoege@compuserve.com",
 	url="http://mdoege.github.io/PySynth/",
-        py_modules=["pysynth", "pysynth_b", "pysynth_c", "pysynth_d", "pysynth_e", "pysynth_p", "pysynth_s", "pysynth_beeper", "play_wav", "mixfiles", "mkfreq", "demosongs"],
+        py_modules=["pysynth", "pysynth_b", "pysynth_c", "pysynth_d", "pysynth_e", "pysynth_p", "pysynth_s", "pysynth_beeper", "pysynth_samp", "play_wav", "mixfiles", "mkfreq", "demosongs"],
 	scripts=["read_abc.py", "readmidi.py", "nokiacomposer2wav.py", "test_nokiacomposer2wav.py", "menv.py"],
 )
 
